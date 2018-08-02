079f9d6088171b8fbda7331e4b639b37c5b28671
unitydemo2 <unitydemo2@yopmail.com>
2018-08-02 08:19:29 +0530

    DW CMS Message

--- DiffPlex/Model/EditLengthResult.cs
+++ DiffPlex/Model/EditLengthResult.cs
@@ -1,4 +1,4 @@
-﻿namespace DiffPlex.Model
+namespace DiffPlex.Model
 {
     public enum Edit
     {
@@ -9,15 +9,28 @@
         InsertUp
     }
 
+    
     public class EditLengthResult
     {
+        /// <summary>
+        /// **Sadasdad**
+        /// </summary>
         public int EditLength { get; set; }
 
+        /// <summary>
+        /// asdasdd
+        /// </summary>
         public int StartX { get; set; }
+        /// <summary>
+        /// <br>*  Dasd</br> <br>*  Asdads</br> <br>*  Sdads</br> <br>*  Sdasd</br> <br>*  Asdad</br> <br>*  rerte</br>
+        /// </summary>
         public int EndX { get; set; }
+        
         public int StartY { get; set; }
+        
         public int EndY { get; set; }
 
+        
         public Edit LastEdit { get; set; }
     }
 }
