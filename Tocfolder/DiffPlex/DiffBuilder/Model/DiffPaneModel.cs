5734ed1891111ef820a6a1c6ea4123c752860b47
unitydemo2 <unitydemo2@yopmail.com>
2018-08-11 16:52:42 +0530

    DW CMS Message

--- Tocfolder/DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
+++ Tocfolder/DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
@@ -1,11 +1,24 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace DiffPlex.DiffBuilder.Model
 {
+    
+    
+    
+    
+    
     public class DiffPaneModel
     {
-        public List<DiffPiece> Lines { get; }
+        /// <summary>
+        /// Test abc ![abc](Enfiled_5b6c27ce5b416027c012a509.jpg) qwe asdfasfa  <dw-code>5b6c27ce5b416027c012a50a<dw-code></dw-code></dw-code>
+        /// </summary>
+                                        public List<DiffPiece> Lines { get; }
 
+        
+        
+        
+        
+        
         public DiffPaneModel()
         {
             Lines = new List<DiffPiece>();
