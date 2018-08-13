d0b4ae7973a95585e4cce34687fcc9cb0f6b93a7
unitydemo2 <35590162+unitydemo2@users.noreply.github.com>
2018-08-13 14:27:33 +0530

    asdfa
    asdfasdfa

--- Tocfolder/DiffPlex/DiffBuilder/Model/DiffPiece.cs
+++ Tocfolder/DiffPlex/DiffBuilder/Model/DiffPiece.cs
@@ -10,7 +10,7 @@ namespace DiffPlex.DiffBuilder.Model
     
     ///     <summary>
     ///      ![abc](Enfiled.jpg)
-    ///     [sample](sample.cs) sbcdfsfssdfgs
+    ///     [sample](sample.cs) sbcdfsfssdfgs asdfasfasfd 
     ///     </summary>
         Unchanged,
     
