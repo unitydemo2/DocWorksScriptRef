85c87c01bb910830de5e3270904def3c6ab92fac
unitydemo2 <unitydemo2@yopmail.com>
2018-08-14 11:49:28 +0530

    DW CMS Message

--- Tocfolder/DiffPlex/DiffBuilder/Model/DiffPiece.cs
+++ Tocfolder/DiffPlex/DiffBuilder/Model/DiffPiece.cs
@@ -132,8 +132,10 @@ namespace DiffPlex.DiffBuilder.Model
   
   
   
+  
   public class DiffPiece
   {
+    
     
     
     
@@ -177,7 +179,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     public int? Position { get; set; }
+    
     
     
     
@@ -219,7 +223,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     public List<DiffPiece> SubPieces { get; set; } = new List<DiffPiece>();
+    
     
     
     
@@ -261,7 +267,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     public string Conflict { get; set; }
+    
     
     
     
@@ -304,6 +312,7 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     public DiffPiece(string text, ChangeType type, int? position = null)
     {
       Text = text;
@@ -315,7 +324,7 @@ namespace DiffPlex.DiffBuilder.Model
     }
 
     ///                     <!-- Badly formed XML comment ignored for member "M:DiffPlex.DiffBuilder.Model.DiffPiece.#ctor" -->
-                                                                                	  
+                                                                                    	  
 		    public DiffPiece()
         : this(null, ChangeType.Imaginary)
     {
