16ccbd4b028f700472d40e4448c6635186ca21f1
unitydemo2 <unitydemo2@yopmail.com>
2018-08-14 11:43:16 +0530

    DW CMS Message

--- Tocfolder/DiffPlex/DiffBuilder/Model/DiffPiece.cs
+++ Tocfolder/DiffPlex/DiffBuilder/Model/DiffPiece.cs
@@ -20,19 +20,22 @@ namespace DiffPlex.DiffBuilder.Model
   
   
   
+  
   public enum ChangeType
   {
     
     ///         <summary>
-    ///          ![abc](Enfiled_5b72693476884f061856f78d.jpg)
-    ///         [5b6c27ce5b416027c012a50a](Examples/sample_5b6c27ce5b416027c012a50a.cs) sasfdasf asfa
+    ///          ![abc](Enfiled_5b72693376884f061856f78b.jpg)
+    ///         [5b72693376884f061856f78c](Examples/sample_5b72693376884f061856f78c.cs) sasfdasf asfa
     ///         </summary>
-            Unchanged,
+                Unchanged,
+    
     
     
     
     
     Deleted,
+    
     
     
     
@@ -70,7 +73,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     Imaginary,
+    
     
     
     
