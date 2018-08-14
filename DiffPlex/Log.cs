68fcfa742987b953fd05739648e895e8a7dbeb3c
msahu-rythmos <msahu@rythmos.com>
2018-08-14 19:21:11 +0530

    Added Delegates for testing

--- DiffPlex/Log.cs
+++ DiffPlex/Log.cs
@@ -15,5 +15,9 @@ namespace DiffPlex
         {
             // not implemented
         }
+
+        public delegate int TestDelegate(string s);
+
+        public delegate void CalculateSquare(double number);
     }
 }
