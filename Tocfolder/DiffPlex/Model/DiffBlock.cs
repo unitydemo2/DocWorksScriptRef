9850ee81b71188c2e6d2ca4cd253c2baae00310c
unitydemo2 <unitydemo2@yopmail.com>
2018-10-04 17:43:38 +0530

    DW CMS Message

--- Tocfolder/DiffPlex/Model/DiffBlock.cs
+++ Tocfolder/DiffPlex/Model/DiffBlock.cs
@@ -1,34 +1,35 @@
-﻿namespace DiffPlex.Model
+namespace DiffPlex.Model
 {
-    /// <summary>
-    /// A block of consecutive edits from A and/or B
-    /// </summary>
-    public class DiffBlock
+    ///     <summary>
+    ///     A block of consecutive edits from A and/or B
+    ///     </summary>
+        public class DiffBlock
     {
-        /// <summary>
-        /// Position where deletions in A begin
-        ///  ![abc](../../../DevImages/Enfiled.jpg)
-        ///  ![abc](Enfiled.jpg)
-        /// [sample](sample.cs)
-        /// </summary>
-        public int DeleteStartA { get; }
+        ///     <summary>
+        ///     Position where deletions in A begin
+        ///      ![abc](../../../DevImages/Enfiled.jpg)
+        ///      ![abc](Enfiled.jpg)
+        ///     [sample](sample.cs)
+        ///     </summary>
+                public int DeleteStartA { get; }
 
-        /// <summary>
-        /// The number of deletions in A
-        /// </summary>
-        public int DeleteCountA { get; }
+        ///     <summary>
+        ///     The number of deletions in A
+        ///     </summary>
+                public int DeleteCountA { get; }
 
-        /// <summary>
-        /// Position where insertion in B begin
-        /// </summary>
-        public int InsertStartB { get; }
+        ///     <summary>
+        ///     Position where insertion in B begin
+        ///     </summary>
+                public int InsertStartB { get; }
 
-        /// <summary>
-        /// The number of insertions in B
-        /// </summary>
-        public int InsertCountB { get; }
+        ///     <summary>
+        ///     The number of insertions in B
+        ///     </summary>
+                public int InsertCountB { get; }
 
 
+        
         public DiffBlock(int deleteStartA, int deleteCountA, int insertStartB, int insertCountB)
         {
             DeleteStartA = deleteStartA;
