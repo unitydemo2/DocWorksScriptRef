986e3909b46d5510d1a0d8b98f450b9f653a07dc
unitydemo2 <unitydemo2@yopmail.com>
2018-10-06 16:20:42 +0530

    DW CMS Message

--- Tocfolder/DiffPlex/Merger.cs
+++ Tocfolder/DiffPlex/Merger.cs
@@ -6,13 +6,13 @@ using DiffPlex.DiffBuilder.Model;
 
 namespace DiffPlex
 {
-    ///     <summary>
-    ///     this is the class for meging 2 texts
-    ///     </summary>
-    ///     <description>
-    ///     merging left and right texts and getting output as merged text
-    ///     </description>
-        public class Merger : IMerger
+    /// <summary>
+    /// this is the class for meging 2 texts
+    /// </summary>
+    /// <description>
+    /// merging left and right texts and getting output as merged text
+    /// </description>
+            public class Merger : IMerger
     {
         ///     <summary>
         ///     enumerator for Merger
@@ -30,48 +30,52 @@ namespace DiffPlex
             MergedText
         }
  
-        ///     <summary>
-        ///     summary for constructor pull test commit test
-        ///     </summary>
-                public Merger()
+        /// <summary>
+        /// summary for constructor pull test commit test
+        /// </summary>
+                        public Merger()
         {
             Console.WriteLine("This is demo constructor");
         }
 
-        ///     <summary>
-        ///     FieldExample1
-        ///     </summary>
-        ///     <description>
-        ///     This is for generating Field example 1
-        ///     </description>
-                public int FieldExample1;
+        /// <summary>
+        /// FieldExample1  This is assets testing 668779  ![abc](test21_5ba4a0ddee4d6b20c491d070.jpg)
+        /// </summary>
+        /// <description>
+        /// This is for generating Field example 1
+        /// </description>
+                        public int FieldExample1;
 
-        ///     <summary>
-        ///     FieldExample2
-        ///     </summary>
-        ///     <description>
-        ///     This is for generating Field example 2
-        ///     </description>
-                public string FieldExample2;
+        /// <summary>
+        /// FieldExample2
+        /// </summary>
+        /// <description>
+        /// This is for generating Field example 2
+        /// </description>
+                        public string FieldExample2;
 
-        ///     <summary>
-        ///     Property 1
-        ///     </summary>
-        ///     <description>
-        ///     This is for generating Property 1
-        ///     </description>
-                public string Property1 { get; set; }
+        /// <summary>
+        /// Property 1
+        /// </summary>
+        /// <description>
+        /// This is for generating Property 1
+        /// </description>
+                        public string Property1 { get; set; }
 
-        ///     <summary>
-        ///     Property 2
-        ///     </summary>
-        ///     <description>
-        ///     This is for generating Property 2
-        ///     </description>
-                public long Property2 { get; set; }
+        /// <summary>
+        /// Property 2
+        /// </summary>
+        /// <description>
+        /// This is for generating Property 2
+        /// </description>
+                        public long Property2 { get; set; }
 
-        /// <!-- Badly formed XML comment ignored for member "M:DiffPlex.Merger.GetMergedText(DiffPlex.DiffBuilder.Model.SideBySideDiffModel,System.String)" -->
-                public static SideBySideDiffModel GetMergedText(SideBySideDiffModel model, string model2)
+        /// <summary>
+        /// </summary>
+        /// <#comment>
+        /// Badly formed XML comment ignored for member "M:DiffPlex.Merger.GetMergedText(DiffPlex.DiffBuilder.Model.SideBySideDiffModel,System.String)"
+        /// </#comment>
+                        public static SideBySideDiffModel GetMergedText(SideBySideDiffModel model, string model2)
         {
             DiffPaneModel mergedText = new DiffPaneModel();
 
@@ -112,12 +116,14 @@ namespace DiffPlex
         }
 
         
+        
         public SideBySideDiffModel GetMergedText(List<string> list)
         {
             //throw new NotImplementedException();
             return new SideBySideDiffModel();
         }
 
+        
         
         public SideBySideDiffModel GetMergedText(SideBySideDiffModel model)
         {
