c979bdcb05c59d9f7c33d299f753b0b6a66330ec
unitydemo2 <unitydemo2@yopmail.com>
2018-10-06 17:18:59 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
+++ DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
@@ -6,12 +6,16 @@ namespace DiffPlex.DiffBuilder.Model
     /// <summary>
     /// This is the summary for DiffPane Model
     /// </summary>
-    public class DiffPaneModel
+        public class DiffPaneModel
     {
         
         
+        /// <summary>
+        /// ![](png_5bb5942b84d9ef020c737cd9.png)
+        /// </summary>
         public List<DiffPiece> Lines { get; }
 
+        
         
         
         public DiffPaneModel()
