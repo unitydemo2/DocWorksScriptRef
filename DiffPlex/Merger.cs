f297202d04c150e84e9105d604da06f618f7441e
unitydemo2 <unitydemo2@yopmail.com>
2018-11-11 18:07:18 +0530

    DW CMS Message

--- DiffPlex/Merger.cs
+++ DiffPlex/Merger.cs
@@ -20,8 +20,9 @@ namespace DiffPlex
         ///     <description>
         ///     this is the enumerator for example
         ///     </description>
-                                                                                public enum MergerEnum
+                                                                                        public enum MergerEnum
         {
+            
             
             
             
@@ -41,7 +42,9 @@ namespace DiffPlex
             
             
             
+            
             RightText,
+            
             
             
             
