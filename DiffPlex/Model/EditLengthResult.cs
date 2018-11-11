bcaa0c49f97a8295a1c8e00ed37ce25c7328da0d
unitydemo2 <unitydemo2@yopmail.com>
2018-11-11 19:34:33 +0530

    DW CMS Message

--- DiffPlex/Model/EditLengthResult.cs
+++ DiffPlex/Model/EditLengthResult.cs
@@ -34,8 +34,10 @@ namespace DiffPlex.Model
     
     
     
+    
     public enum Edit
     {
+        
         
         
         
@@ -105,7 +107,9 @@ namespace DiffPlex.Model
         
         
         
+        
         DeleteRight,
+        
         
         
         
@@ -175,7 +179,9 @@ namespace DiffPlex.Model
         
         
         
+        
         InsertDown,
+        
         
         
         
