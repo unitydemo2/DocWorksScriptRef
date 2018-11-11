5928038fda2c5b3ff11291a70c4ec91e72b9f6cc
unitydemo2 <unitydemo2@yopmail.com>
2018-11-11 18:50:24 +0530

    DW CMS Message

--- DiffPlex/Differ.cs
+++ DiffPlex/Differ.cs
@@ -25,6 +25,7 @@ namespace DiffPlex
     
     
     
+    
     [Obsolete]
     public class Differ : IDiffer
     {
@@ -33,9 +34,10 @@ namespace DiffPlex
         ///     <summary>
         ///     this is the property for 
         ///     </summary>
-                                                                                                                                                                                [Obsolete]
+                                                                                                                                                                                        [Obsolete]
         public string obsoletedPropertyForTesting { get; set; }
 
+        
         
         
         
@@ -63,7 +65,7 @@ namespace DiffPlex
         }
 
         /// <!-- Badly formed XML comment ignored for member "M:DiffPlex.Differ.CreateLineDiffs(System.String,System.String,System.Boolean,System.Boolean)" -->
-                                                                                                                                                                                [Obsolete]
+                                                                                                                                                                                        [Obsolete]
         public DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase)
         {
             if (oldText == null) throw new ArgumentNullException(nameof(oldText));
@@ -92,11 +94,13 @@ namespace DiffPlex
         
         
         
+        
         public DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace)
         {
             return CreateCharacterDiffs(oldText, newText, ignoreWhitespace, false);
         }
 
+        
         
         
         
@@ -158,11 +162,13 @@ namespace DiffPlex
         
         
         
+        
         public DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, char[] separators)
         {
             return CreateWordDiffs(oldText, newText, ignoreWhitespace, false, separators);
         }
 
+        
         
         
         
@@ -219,11 +225,13 @@ namespace DiffPlex
         
         
         
+        
         public DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, Func<string, string[]> chunker)
         {
             return CreateCustomDiffs(oldText, newText, ignoreWhiteSpace, false, chunker);
         }
 
+        
         
         
         
@@ -326,7 +334,7 @@ namespace DiffPlex
         ///     <param name="startB">lower bound inclusive</param>
         ///     <param name="endB">upper bound exclusive</param>
         ///     <returns></returns>
-                                                                                                                                                                                protected static EditLengthResult CalculateEditLength(int[] A, int startA, int endA, int[] B, int startB, int endB)
+                                                                                                                                                                                        protected static EditLengthResult CalculateEditLength(int[] A, int startA, int endA, int[] B, int startB, int endB)
         {
             int N = endA - startA;
             int M = endB - startB;
@@ -474,6 +482,7 @@ namespace DiffPlex
             throw new Exception("Should never get here");
         }
 
+        
         
         
         
