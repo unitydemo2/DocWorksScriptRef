dfb1bcb269fabe18442f35f833107ddf95343073
unitydemo2 <unitydemo2@yopmail.com>
2018-11-12 21:30:04 +0530

    DW CMS Message

--- DiffPlex/Model/ModificationData.cs
+++ DiffPlex/Model/ModificationData.cs
@@ -147,8 +147,10 @@ namespace DiffPlex.Model
     
     
     
+    
     public class ModificationData
     {
+        
         
         
         
@@ -445,8 +447,10 @@ namespace DiffPlex.Model
         
         
         
+        
         public string RawData { get; }
 
+        
         
         
         
@@ -743,8 +747,10 @@ namespace DiffPlex.Model
         
         
         
+        
         public string[] Pieces { get; set; }
 
+        
         
         
         
