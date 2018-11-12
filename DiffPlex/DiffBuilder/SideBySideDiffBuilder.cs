05b04e618e7daed360bf51a0b5dbea47cc6eaeae
unitydemo2 <unitydemo2@yopmail.com>
2018-11-12 09:45:18 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/SideBySideDiffBuilder.cs
+++ DiffPlex/DiffBuilder/SideBySideDiffBuilder.cs
@@ -50,12 +50,14 @@ namespace DiffPlex.DiffBuilder
     
     
     
+    
     public class SideBySideDiffBuilder : ISideBySideDiffBuilder
     {
         private readonly IDiffer differ;
 
         private delegate void PieceBuilder(string oldText, string newText, List<DiffPiece> oldPieces, List<DiffPiece> newPieces);
 
+        
         
         
         
@@ -148,11 +150,13 @@ namespace DiffPlex.DiffBuilder
         
         
         
+        
         public SideBySideDiffBuilder(IDiffer differ)
         {
             this.differ = differ ?? throw new ArgumentNullException(nameof(differ));
         }
 
+        
         
         
         
