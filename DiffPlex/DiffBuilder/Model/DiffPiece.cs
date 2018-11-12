537a831659f1977d5575c77a63f3907d0ff1f4f2
unitydemo2 <unitydemo2@yopmail.com>
2018-11-12 16:16:01 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/DiffPiece.cs
+++ DiffPlex/DiffBuilder/Model/DiffPiece.cs
@@ -120,8 +120,10 @@ namespace DiffPlex.DiffBuilder.Model
   
   
   
+  
   public enum ChangeType
   {
+    
     
     
     
@@ -359,7 +361,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     Deleted,
+    
     
     
     
@@ -597,7 +601,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     Imaginary,
+    
     
     
     
