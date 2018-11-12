ecf52eef809713dea5a2e5dd03db89cb5c6a5984
unitydemo2 <unitydemo2@yopmail.com>
2018-11-12 13:39:05 +0530

    DW CMS Message

--- DiffPlex/Merger.cs
+++ DiffPlex/Merger.cs
@@ -20,8 +20,9 @@ namespace DiffPlex
         ///     <description>
         ///     this is the enumerator for example
         ///     </description>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        public enum MergerEnum
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                public enum MergerEnum
         {
+            
             
             
             
@@ -211,7 +212,9 @@ namespace DiffPlex
             
             
             
+            
             RightText,
+            
             
             
             
