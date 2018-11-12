547b70a578bcee5883294eaabc08ee4c76e8a29c
unitydemo2 <unitydemo2@yopmail.com>
2018-11-12 17:19:03 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/InlineDiffBuilder.cs
+++ DiffPlex/DiffBuilder/InlineDiffBuilder.cs
@@ -130,10 +130,12 @@ namespace DiffPlex.DiffBuilder
     
     
     
+    
     public class InlineDiffBuilder : IInlineDiffBuilder
     {
         private readonly IDiffer differ;
 
+        
         
         
         
@@ -264,6 +266,7 @@ namespace DiffPlex.DiffBuilder
             this.differ = differ ?? throw new ArgumentNullException(nameof(differ));
         }
 
+        
         
         
         
