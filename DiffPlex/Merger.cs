8d0bd1e36e9bf0b3f5c247ae0d794a629ca123d9
unitydemo2 <unitydemo2@yopmail.com>
2018-11-12 12:23:15 +0530

    DW CMS Message

--- DiffPlex/Merger.cs
+++ DiffPlex/Merger.cs
@@ -12,7 +12,7 @@ namespace DiffPlex
     ///     <description>
     ///     merging left and right texts and getting output as merged text
     ///     </description>
-                                                                                                                                                                                                                                                                                                                                            public class Merger : IMerger
+                                                                                                                                                                                                                                                                                                                                                public class Merger : IMerger
     {
         ///     <summary>
         ///     enumerator for Merger
@@ -279,7 +279,7 @@ namespace DiffPlex
         ///     <summary>
         ///     summary for constructor pull test commit test
         ///     </summary>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        public Merger()
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                public Merger()
         {
             Console.WriteLine("This is demo constructor");
         }
@@ -290,7 +290,7 @@ namespace DiffPlex
         ///     <description>
         ///     This is for generating Field example 1
         ///     </description>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        public int FieldExample1;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                public int FieldExample1;
 
         ///     <summary>
         ///     FieldExample2
@@ -298,7 +298,7 @@ namespace DiffPlex
         ///     <description>
         ///     This is for generating Field example 2
         ///     </description>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        public string FieldExample2;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                public string FieldExample2;
 
         ///     <summary>
         ///     Property 1
@@ -306,7 +306,7 @@ namespace DiffPlex
         ///     <description>
         ///     This is for generating Property 1
         ///     </description>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        public string Property1 { get; set; }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                public string Property1 { get; set; }
 
         ///     <summary>
         ///     Property 2
@@ -314,10 +314,10 @@ namespace DiffPlex
         ///     <description>
         ///     This is for generating Property 2
         ///     </description>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        public long Property2 { get; set; }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                public long Property2 { get; set; }
 
         /// <!-- Badly formed XML comment ignored for member "M:DiffPlex.Merger.GetMergedText(DiffPlex.DiffBuilder.Model.SideBySideDiffModel,System.String)" -->
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        public static SideBySideDiffModel GetMergedText(SideBySideDiffModel model, string model2)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                public static SideBySideDiffModel GetMergedText(SideBySideDiffModel model, string model2)
         {
             DiffPaneModel mergedText = new DiffPaneModel();
 
@@ -439,12 +439,14 @@ namespace DiffPlex
         
         
         
+        
         public SideBySideDiffModel GetMergedText(List<string> list)
         {
             //throw new NotImplementedException();
             return new SideBySideDiffModel();
         }
 
+        
         
         
         
