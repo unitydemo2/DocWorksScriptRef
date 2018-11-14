9a61920ff89a167e1b4449026355a74719135e6c
unitydemo2 <unitydemo2@yopmail.com>
2018-11-14 06:17:33 +0530

    DW CMS Message

--- DiffPlex/IMerger.cs
+++ DiffPlex/IMerger.cs
@@ -245,6 +245,7 @@ namespace DiffPlex
     
     
     
+    
     public interface IMerger
     {
         
@@ -484,8 +485,10 @@ namespace DiffPlex
         
         
         
+        
         SideBySideDiffModel GetMergedText(List<string> list);
 
+        
         
         
         
