27b8d296f58bca7eb8e1b2d8b7b8b36e75a3cf75
unitydemo2 <unitydemo2@yopmail.com>
2018-11-16 02:05:38 +0530

    DW CMS Message

--- DiffPlex/Log.cs
+++ DiffPlex/Log.cs
@@ -353,8 +353,10 @@ namespace DiffPlex
     
     
     
+    
     public static class Log
     {
+        
         
         
         
@@ -712,6 +714,7 @@ namespace DiffPlex
             Debug.WriteLine(string.Format(format, args));
         }
 
+        
         
         
         
