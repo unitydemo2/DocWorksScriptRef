1acbb4e0e8ff96df96be4b7eda9af4465d19a757
CMSQATeam <cmsqa123@gmail.com>
2018-11-16 16:45:38 +0530

    DW CMS Message

--- DiffPlex/Model/ModificationData.cs
+++ DiffPlex/Model/ModificationData.cs
@@ -76,8 +76,10 @@ namespace DiffPlex.Model
     
     
     
+    
     public class ModificationData
     {
+        
         
         
         
@@ -232,8 +234,10 @@ namespace DiffPlex.Model
         
         
         
+        
         public string RawData { get; }
 
+        
         
         
         
@@ -388,8 +392,10 @@ namespace DiffPlex.Model
         
         
         
+        
         public string[] Pieces { get; set; }
 
+        
         
         
         
