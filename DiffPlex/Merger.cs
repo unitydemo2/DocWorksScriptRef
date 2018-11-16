eff7259f021469fe69662186cdc9945bfb49434a
CMSQATeam <cmsqa123@gmail.com>
2018-11-17 01:54:09 +0530

    DW CMS Message

--- DiffPlex/Merger.cs
+++ DiffPlex/Merger.cs
@@ -20,8 +20,9 @@ namespace DiffPlex
         ///     <description>
         ///     this is the enumerator for example
         ///     </description>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                public enum MergerEnum
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        public enum MergerEnum
         {
+            
             
             
             
@@ -345,7 +346,9 @@ namespace DiffPlex
             
             
             
+            
             RightText,
+            
             
             
             
