bb27a33488a50843e5be83cf3ac1189781a42603
CMSQATeam <cmsqa123@gmail.com>
2018-11-16 17:52:30 +0530

    DW CMS Message

--- DiffPlex/IMerger.cs
+++ DiffPlex/IMerger.cs
@@ -97,6 +97,7 @@ namespace DiffPlex
     
     
     
+    
     public interface IMerger
     {
         
@@ -188,8 +189,10 @@ namespace DiffPlex
         
         
         
+        
         SideBySideDiffModel GetMergedText(List<string> list);
 
+        
         
         
         
