2cc6c3b7c69a9c96a1ff773d5cb4a4bb7a0ae9d3
CMSQATeam <cmsqa123@gmail.com>
2018-11-16 14:36:48 +0530

    DW CMS Message

--- DiffPlex/Merger.cs
+++ DiffPlex/Merger.cs
@@ -20,8 +20,9 @@ namespace DiffPlex
         ///     <description>
         ///     this is the enumerator for example
         ///     </description>
-                                                                                                                                                                                                                                                                                                                                                                                public enum MergerEnum
+                                                                                                                                                                                                                                                                                                                                                                                        public enum MergerEnum
         {
+            
             
             
             
@@ -113,7 +114,9 @@ namespace DiffPlex
             
             
             
+            
             RightText,
+            
             
             
             
