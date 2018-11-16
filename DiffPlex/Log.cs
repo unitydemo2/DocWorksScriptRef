0dc7ac26d79ca7211de3a95b74be801cb6aa6feb
CMSQATeam <cmsqa123@gmail.com>
2018-11-17 04:28:06 +0530

    DW CMS Message

--- DiffPlex/Log.cs
+++ DiffPlex/Log.cs
@@ -179,8 +179,10 @@ namespace DiffPlex
     
     
     
+    
     public static class Log
     {
+        
         
         
         
@@ -364,6 +366,7 @@ namespace DiffPlex
             Debug.WriteLine(string.Format(format, args));
         }
 
+        
         
         
         
