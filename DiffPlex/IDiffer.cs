1edcfbbeaa973a7feb95e5207009c9c0b0d24b86
CMSQATeam <cmsqa123@gmail.com>
2018-11-16 18:29:50 +0530

    DW CMS Message

--- DiffPlex/IDiffer.cs
+++ DiffPlex/IDiffer.cs
@@ -6,8 +6,9 @@ namespace DiffPlex
     ///     <summary>
     ///     Provides methods for generate differences between texts
     ///     </summary>
-                                                                                                                                                                                                                                                                                                                                                                                                    public interface IDiffer
+                                                                                                                                                                                                                                                                                                                                                                                                        public interface IDiffer
     {
+        
         
         
         
@@ -201,7 +202,9 @@ namespace DiffPlex
         
         
         
+        
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
+        
         
         
         
@@ -395,7 +398,9 @@ namespace DiffPlex
         
         
         
+        
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
+        
         
         
         
@@ -589,6 +594,7 @@ namespace DiffPlex
         
         
         
+        
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase, char[] separators);
         
         
@@ -686,7 +692,9 @@ namespace DiffPlex
         
         
         
+        
         DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, Func<string, string[]> chunker);
+        
         
         
         
