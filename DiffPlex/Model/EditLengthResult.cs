4e257292e03f4e3550428d2c840d4c8c90cc8437
CMSQATeam <cmsqa123@gmail.com>
2018-11-17 21:48:55 +0530

    DW CMS Message

--- DiffPlex/Model/EditLengthResult.cs
+++ DiffPlex/Model/EditLengthResult.cs
@@ -255,8 +255,10 @@ namespace DiffPlex.Model
     
     
     
+    
     public enum Edit
     {
+        
         
         
         
@@ -768,7 +770,9 @@ namespace DiffPlex.Model
         
         
         
+        
         DeleteRight,
+        
         
         
         
@@ -1280,7 +1284,9 @@ namespace DiffPlex.Model
         
         
         
+        
         InsertDown,
+        
         
         
         
