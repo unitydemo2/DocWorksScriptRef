7e8670fbebb6338c27d4247e8428014f605d124a
CMSQATeam <cmsqa123@gmail.com>
2018-11-17 17:38:18 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/SideBySideDiffBuilder.cs
+++ DiffPlex/DiffBuilder/SideBySideDiffBuilder.cs
@@ -367,12 +367,14 @@ namespace DiffPlex.DiffBuilder
     
     
     
+    
     public class SideBySideDiffBuilder : ISideBySideDiffBuilder
     {
         private readonly IDiffer differ;
 
         private delegate void PieceBuilder(string oldText, string newText, List<DiffPiece> oldPieces, List<DiffPiece> newPieces);
 
+        
         
         
         
@@ -1099,11 +1101,13 @@ namespace DiffPlex.DiffBuilder
         
         
         
+        
         public SideBySideDiffBuilder(IDiffer differ)
         {
             this.differ = differ ?? throw new ArgumentNullException(nameof(differ));
         }
 
+        
         
         
         
