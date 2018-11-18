955e782097bdfb3efff794fd20c472b00078eb28
CMSQATeam <cmsqa123@gmail.com>
2018-11-18 08:51:36 +0530

    DW CMS Message

--- DiffPlex/Model/EditLengthResult.cs
+++ DiffPlex/Model/EditLengthResult.cs
@@ -2017,8 +2017,10 @@ namespace DiffPlex.Model
     
     
     
+    
     public class EditLengthResult
     {
+        
         
         
         
@@ -2593,7 +2595,9 @@ namespace DiffPlex.Model
         
         
         
+        
         public int StartX { get; set; }
+        
         
         
         
@@ -3167,7 +3171,9 @@ namespace DiffPlex.Model
         
         
         
+        
         public int StartY { get; set; }
+        
         
         
         
@@ -3456,6 +3462,7 @@ namespace DiffPlex.Model
         
         public int EndY { get; set; }
 
+        
         
         
         
