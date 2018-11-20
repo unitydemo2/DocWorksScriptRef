6e4aed2fce6911c6c4b4f612ee3add767893c4ce
CMSQATeam <cmsqa123@gmail.com>
2018-11-20 13:54:17 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/SideBySideDiffModel.cs
+++ DiffPlex/DiffBuilder/Model/SideBySideDiffModel.cs
@@ -3,8 +3,9 @@ namespace DiffPlex.DiffBuilder.Model
     ///     <summary>
     ///     A model which represents differences between to texts to be shown side by side
     ///     </summary>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        public class SideBySideDiffModel
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            public class SideBySideDiffModel
     {
+        
         
         
         
@@ -1200,7 +1201,9 @@ namespace DiffPlex.DiffBuilder.Model
         
         
         
+        
         public DiffPaneModel NewText { get; }
+        
         
         
         
@@ -1800,6 +1803,7 @@ namespace DiffPlex.DiffBuilder.Model
         
         public DiffPaneModel MergedText { get; set; }
 
+        
         
         
         
