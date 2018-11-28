036ad26985b1ae07d9e17178e457351ba4c35a12
CMSQATeam <cmsqa123@gmail.com>
2018-11-28 18:29:48 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
+++ DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
@@ -511,6 +511,7 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     public class DiffPaneModel
     {
         
@@ -1022,8 +1023,10 @@ namespace DiffPlex.DiffBuilder.Model
         
         
         
+        
         public List<DiffPiece> Lines { get; }
 
+        
         
         
         
