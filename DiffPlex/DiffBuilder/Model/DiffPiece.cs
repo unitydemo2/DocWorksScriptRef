1d7966c8ea59adec2b73443681f2b01b3fdfec89
CMSQATeam <cmsqa123@gmail.com>
2018-12-02 12:23:36 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/DiffPiece.cs
+++ DiffPlex/DiffBuilder/Model/DiffPiece.cs
@@ -1158,8 +1158,10 @@ namespace DiffPlex.DiffBuilder.Model
   
   
   
+  
   public enum ChangeType
   {
+    
     
     
     
@@ -3473,7 +3475,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     Deleted,
+    
     
     
     
@@ -5787,7 +5791,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     Imaginary,
+    
     
     
     
