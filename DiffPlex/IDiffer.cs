f263386f3b6e40ef487677a69f9b0bcacaba0cac
CMSQATeam <cmsqa123@gmail.com>
2018-12-03 05:35:22 +0530

    DW CMS Message

--- DiffPlex/IDiffer.cs
+++ DiffPlex/IDiffer.cs
@@ -6,8 +6,9 @@ namespace DiffPlex
     ///     <summary>
     ///     Provides methods for generate differences between texts
     ///     </summary>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            public interface IDiffer
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                public interface IDiffer
     {
+        
         
         
         
@@ -1125,7 +1126,9 @@ namespace DiffPlex
         
         
         
+        
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
+        
         
         
         
@@ -2243,7 +2246,9 @@ namespace DiffPlex
         
         
         
+        
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
+        
         
         
         
@@ -3361,6 +3366,7 @@ namespace DiffPlex
         
         
         
+        
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase, char[] separators);
         
         
@@ -3920,7 +3926,9 @@ namespace DiffPlex
         
         
         
+        
         DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, Func<string, string[]> chunker);
+        
         
         
         
