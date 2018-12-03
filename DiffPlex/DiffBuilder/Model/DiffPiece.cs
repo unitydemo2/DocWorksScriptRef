e13a5590a88b49296555d4ea85bde800a0e85358
CMSQATeam <cmsqa123@gmail.com>
2018-12-03 10:04:50 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/DiffPiece.cs
+++ DiffPlex/DiffBuilder/Model/DiffPiece.cs
@@ -8363,8 +8363,10 @@ namespace DiffPlex.DiffBuilder.Model
   
   
   
+  
   public class DiffPiece
   {
+    
     
     
     
@@ -10742,7 +10744,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     public int? Position { get; set; }
+    
     
     
     
@@ -13120,7 +13124,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     public List<DiffPiece> SubPieces { get; set; } = new List<DiffPiece>();
+    
     
     
     
@@ -15498,7 +15504,9 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     public string Conflict { get; set; }
+    
     
     
     
@@ -17877,6 +17885,7 @@ namespace DiffPlex.DiffBuilder.Model
     
     
     
+    
     public DiffPiece(string text, ChangeType type, int? position = null)
     {
       Text = text;
@@ -17887,6 +17896,7 @@ namespace DiffPlex.DiffBuilder.Model
       BorderStyle = null;
     }
 
+    
     
     
     
