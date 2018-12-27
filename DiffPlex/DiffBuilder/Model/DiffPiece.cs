5520ab108095fe039b433ff5731041a876befffe
msahu-rythmos <msahu@rythmos.com>
2018-12-27 15:19:23 +0530

    preprocessor data

--- DiffPlex/DiffBuilder/Model/DiffPiece.cs
+++ DiffPlex/DiffBuilder/Model/DiffPiece.cs
@@ -2,38 +2,42 @@ using System.Collections.Generic;
 
 namespace DiffPlex.DiffBuilder.Model
 {
-  public enum ChangeType
-  {
-    Unchanged,
-    Deleted,
-    Inserted,
-    Imaginary,
-    Modified
-  }
-
-  public class DiffPiece
-  {
-    public ChangeType Type { get; set; }
-    public int? Position { get; set; }
-    public string Text { get; set; }
-    public List<DiffPiece> SubPieces { get; set; } = new List<DiffPiece>();
-    public string Group { get; set; }
-    public string Conflict { get; set; }
-    public string BorderStyle { get; set; }
-
-    public DiffPiece(string text, ChangeType type, int? position = null)
+    public enum ChangeType
     {
-      Text = text;
-      Position = position;
-      Type = type;
-      Group = null;
-      Conflict = null;
-      BorderStyle = null;
+        Unchanged,
+        Deleted,
+        Inserted,
+        Imaginary,
+        Modified
     }
 
-    public DiffPiece()
-        : this(null, ChangeType.Imaginary)
+    public class DiffPiece
     {
+        public ChangeType Type { get; set; }
+        public int? Position { get; set; }
+        public string Text { get; set; }
+        public List<DiffPiece> SubPieces { get; set; } = new List<DiffPiece>();
+        public string Group { get; set; }
+        public string Conflict { get; set; }
+        public string BorderStyle { get; set; }
+
+#if Win64
+        public string PropertyForWindows { get; set; }
+#endif
+
+        public DiffPiece(string text, ChangeType type, int? position = null)
+        {
+            Text = text;
+            Position = position;
+            Type = type;
+            Group = null;
+            Conflict = null;
+            BorderStyle = null;
+        }
+
+        public DiffPiece()
+            : this(null, ChangeType.Imaginary)
+        {
+        }
     }
-  }
 }
