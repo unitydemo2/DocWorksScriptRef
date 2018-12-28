94e8d6ae3cbe32b187a057375db39b5961d9d843
unimo <35590162+unitydemo2@users.noreply.github.com>
2018-12-29 03:01:55 +0530

    Update SideBySideDiffModel.cs

--- DiffPlex/DiffBuilder/Model/SideBySideDiffModel.cs
+++ DiffPlex/DiffBuilder/Model/SideBySideDiffModel.cs
@@ -1,7 +1,7 @@
 ﻿namespace DiffPlex.DiffBuilder.Model
 {
     /// <summary>
-    /// A model which represents differences between to texts to be shown side by side MUKHERJEE 291220180227
+    /// A model which represents differences between to texts to be shown side by side MUKHERJEE 291220180301
     /// </summary>
     public class SideBySideDiffModel
     {
