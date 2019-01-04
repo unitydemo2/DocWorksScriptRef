8b7475239721b48baa19abfb6309d0cc92cc6851
unimo <35590162+unitydemo2@users.noreply.github.com>
2019-01-04 13:10:39 +0530

    Create IDiffer901.cs

---
+++ DiffPlex/IDiffer901.cs
@@ -0,0 +1,14 @@
+using System;
+using DiffPlex.Model;
+
+namespace DiffPlex
+{
+    /// <summary>
+    /// Provides methods for generate differences between texts
+    /// </summary>
+    public interface IDiffer901
+    {
+        DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace);
+        DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
+    }
+}
