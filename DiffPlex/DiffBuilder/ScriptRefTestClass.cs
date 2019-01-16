06f4e39bd4aadf7ff4d03cc3ef6e4558af3fbc13
unitydemo2 <unitydemo2@yopmail.com>
2019-01-16 12:41:30 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/ScriptRefTestClass.cs
+++ DiffPlex/DiffBuilder/ScriptRefTestClass.cs
@@ -3,8 +3,10 @@ using DiffPlex.DiffBuilder.Model;
 namespace DiffPlex.DiffBuilder
 {
     
+    
     public class ScriptRefTestClass : ClassProperty, IScriptRefTestClass
     {
+        
         
         public int AddThreeNumbers(int a, int b, int c)
         {
