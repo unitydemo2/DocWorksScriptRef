82c94e2dc48c67d748e9d8997fdb9a22aa9b7244
unitydemo2 <unitydemo2@yopmail.com>
2019-01-16 12:30:20 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/DiffPiece.cs
+++ DiffPlex/DiffBuilder/Model/DiffPiece.cs
@@ -18,29 +18,39 @@ namespace DiffPlex.DiffBuilder.Model
     }
 
     
+    
     public class DiffPiece
     {
         
+        
         public ChangeType Type { get; set; }
+        
         
         public int? Position { get; set; }
         
+        
         public string Text { get; set; }
+        
         
         public List<DiffPiece> SubPieces { get; set; } = new List<DiffPiece>();
         
+        
         public string Group { get; set; }
         
+        
         public string Conflict { get; set; }
+        
         
         public string BorderStyle { get; set; }
 
 #if Win64
         
+
 public string PropertyForWindows { get; set; }
 #endif
 
         
+
 public DiffPiece(string text, ChangeType type, int? position = null)
         {
             Text = text;
@@ -51,6 +61,7 @@ public DiffPiece(string text, ChangeType type, int? position = null)
             BorderStyle = null;
         }
 
+        
         
         public DiffPiece()
             : this(null, ChangeType.Imaginary)
