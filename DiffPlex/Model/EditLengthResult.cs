a5f7d06e5dedfee18725acfd5bb1609264500eb1
unitydemo2 <unitydemo2@yopmail.com>
2019-01-16 12:41:35 +0530

    DW CMS Message

--- DiffPlex/Model/EditLengthResult.cs
+++ DiffPlex/Model/EditLengthResult.cs
@@ -16,20 +16,27 @@ namespace DiffPlex.Model
     }
 
     
+    
     public class EditLengthResult
     {
+        
         
         public int EditLength { get; set; }
 
         
+        
         public int StartX { get; set; }
+        
         
         public int EndX { get; set; }
         
+        
         public int StartY { get; set; }
+        
         
         public int EndY { get; set; }
 
+        
         
         public Edit LastEdit { get; set; }
     }
