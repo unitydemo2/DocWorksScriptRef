53ae59bef5d2e23b6443494e79f347e7ab4576a5
unitydemo2 <unitydemo2@yopmail.com>
2019-01-16 12:41:36 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/IInlineDiffBuilder.cs
+++ DiffPlex/DiffBuilder/IInlineDiffBuilder.cs
@@ -4,8 +4,10 @@ namespace DiffPlex.DiffBuilder
 {
     
     
+    
     public interface IInlineDiffBuilder
     {
+        
         
         
         DiffPaneModel BuildDiffModel(string oldText, string newText);
