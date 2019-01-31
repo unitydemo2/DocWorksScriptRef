01b956e3b74fad46d9ef2fcc195318fe50d7ab1d
unimo <35590162+unitydemo2@users.noreply.github.com>
2019-01-31 13:40:21 +0530

    Create SomeData.cs

---
+++ DiffPlex/Model/SomeData.cs
@@ -0,0 +1,11 @@
+namespace DiffPlex.Model
+{
+    public class SomeData
+    { 
+        public string RawData { get; }
+        public SomeData(string str)
+        {
+            RawData = str;
+        }
+    }
+}
