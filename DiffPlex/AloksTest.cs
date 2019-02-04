07f0047ea19ef4f75e0a88dfc596c8c2797de674
CMSQATeam <cmsqa123@gmail.com>
2019-02-04 12:32:44 +0530

    DW CMS Message

--- DiffPlex/AloksTest.cs
+++ DiffPlex/AloksTest.cs
@@ -3,17 +3,27 @@ using System;
 namespace DiffPlex
 {
     
+    /// <summary>
+    /// </summary>
+    /// <description>
+    /// </description>
     [Obsolete]
     public class AloksTest
     {
         private static readonly string[] emptyStringArray = new string[0];
-        ///     <summary>
-                ///     this is the property for 
-                ///     </summary>
-                        [Obsolete]
+        /// <summary>
+                                /// this is the property for
+                                /// </summary>
+                                /// <description>
+                                /// </description>
+                                                        [Obsolete]
         public string obsoletedPropertyForTesting { get; set; }
 
         
+        /// <summary>
+        /// </summary>
+        /// <description>
+        /// </description>
         public string CompareString(string oldText, string newText, bool ignoreWhitespace)
         {
             if (oldText.Equals(newText))
@@ -22,14 +32,18 @@ namespace DiffPlex
                 return "not equal";
         }
 
-        ///     <summary>
-                ///     this method is to find diffs in 2 texts
-                ///     </summary>
-                ///     <param name="oldText">this param is for existing text</param>
-                ///     <param name="newText">this param is for updated text</param>
-                ///     <param name="ignoreWhitespace"></param>
-                ///     <param name="ignoreCase"></param>
-                        [Obsolete]
+        /// <summary>
+                                /// this method is to find diffs in 2 texts
+                                /// </summary>
+                                /// <description>
+                                /// </description>
+                                /// <param name="oldText">
+                                /// this param is for existing text
+                                /// </param>
+                                /// <param name="newText">
+                                /// this param is for updated text
+                                /// </param>
+                                                        [Obsolete]
         public bool CompareStringLength(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase)
         {
             if (oldText == null) throw new ArgumentNullException(nameof(oldText));
@@ -38,6 +52,10 @@ namespace DiffPlex
         }
 
         
+        /// <summary>
+        /// </summary>
+        /// <description>
+        /// </description>
         public string CreateSomeOutput(string oldText, string newText, bool ignoreWhitespace)
         {
             return "Test Output";
