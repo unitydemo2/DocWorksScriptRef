00615cf6a3d49e76fc3d2933a4029b9aa292eebc
CMSQATeam <cmsqa123@gmail.com>
2019-02-07 16:01:42 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
+++ DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 namespace DiffPlex.DiffBuilder.Model
 {
     
+    
     public class DiffPaneModel
     {
         
+        
         public List<DiffPiece> Lines { get; }
 
+        
         
         public DiffPaneModel()
         {
