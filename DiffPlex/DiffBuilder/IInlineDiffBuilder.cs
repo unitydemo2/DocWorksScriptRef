858afc593ad91e7f71a7fe41e0d2526111dfd2b0
CMSQATeam <cmsqa123@gmail.com>
2019-02-08 15:35:55 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/IInlineDiffBuilder.cs
+++ DiffPlex/DiffBuilder/IInlineDiffBuilder.cs
@@ -3,8 +3,10 @@ using DiffPlex.DiffBuilder.Model;
 namespace DiffPlex.DiffBuilder
 {
     
+    
     public interface IInlineDiffBuilder
     {
+        
         
         DiffPaneModel BuildDiffModel(string oldText, string newText);
     }
