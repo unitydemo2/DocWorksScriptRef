34d4c609f0278d24a099cc9768a105078b398751
CMSQATeam <cmsqa123@gmail.com>
2019-02-08 15:36:02 +0530

    DW CMS Message

--- DiffPlex/Model/DiffResult.cs
+++ DiffPlex/Model/DiffResult.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 namespace DiffPlex.Model
 {
     ///     <summary>
-        ///     The result of diffing two peices of text
-        ///     </summary>
-            public class DiffResult
+                ///     The result of diffing two peices of text
+                ///     </summary>
+                            public class DiffResult
     {
         ///     <summary>
-                ///     The chunked peices of the old text
-                ///     </summary>
-                        public string[] PiecesOld { get; }
+                                ///     The chunked peices of the old text
+                                ///     </summary>
+                                                        public string[] PiecesOld { get; }
 
         ///     <summary>
-                ///     The chunked peices of the new text
-                ///     </summary>
-                        public string[] PiecesNew { get; }
+                                ///     The chunked peices of the new text
+                                ///     </summary>
+                                                        public string[] PiecesNew { get; }
 
+        
         
         protected int ProtectedProperty { get; set; }
 
         
+        
         public static int StaticProperty { get; set; }
 
+        
         
         protected void MethodA()
         {
@@ -35,11 +38,13 @@ namespace DiffPlex.Model
         }
 
         
+        
         public static bool operator !=(DiffResult a, DiffResult b)
         {
             return true;
         }
 
+        
         
         public static bool operator ==(DiffResult a, DiffResult b)
         {
@@ -47,10 +52,11 @@ namespace DiffPlex.Model
         }
 
         ///     <summary>
-                ///     A collection of DiffBlocks which details deletions and insertions
-                ///     </summary>
-                        public IList<DiffBlock> DiffBlocks { get; set; }
+                                ///     A collection of DiffBlocks which details deletions and insertions
+                                ///     </summary>
+                                                        public IList<DiffBlock> DiffBlocks { get; set; }
 
+        
         
         public DiffResult(string[] peicesOld, string[] piecesNew, IList<DiffBlock> blocks)
         {
