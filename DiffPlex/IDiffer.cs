8c306691fac4ad55213a10880913e12ab598b01a
CMSQATeam <cmsqa123@gmail.com>
2019-02-16 13:01:53 +0530

    DW CMS Message

--- DiffPlex/IDiffer.cs
+++ DiffPlex/IDiffer.cs
@@ -4,30 +4,40 @@ using DiffPlex.Model;
 namespace DiffPlex
 {
     /// <summary>
-                /// Provides methods for generate differences between texts
-                /// </summary>
-                /// <description>
-                /// ![](angeler_5c6795831b16f52820f887aa.jpeg)<br/> <br/>  []()<br/>
-                /// </description>
-                            public interface IDiffer
+                                /// Provides methods for generate differences between texts
+                                /// </summary>
+                                /// <description>
+                                /// ![](angeler_5c6795831b16f52820f887aa.jpeg)<br/> <br/> <br/> <br/>  []()
+                                /// </description>
+                                                            public interface IDiffer
     {
         
         /// <summary>
-        /// [5c679f581b16f52820f89164](adding_5c679f581b16f52820f89164.cs)
+                /// [5c679f581b16f52820f89164](adding_5c679f581b16f52820f89164.cs)
+                /// </summary>
+                /// <description>
+                /// This is sample description
+                /// </description>
+                        DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace);
+        
+        
+        /// <summary>
+        /// This is another description
         /// </summary>
-        DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace);
-        
-        
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
+        
         
         
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace);
         
         
+        
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
         
         
+        
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, char[] separators);
+        
         
         
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase, char[] separators);
