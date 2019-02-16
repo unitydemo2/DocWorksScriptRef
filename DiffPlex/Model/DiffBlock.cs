3a6f3890fd37102625a53acac35fa05dfb6e498a
CMSQATeam <cmsqa123@gmail.com>
2019-02-16 16:12:15 +0530

    DW CMS Message

--- DiffPlex/Model/DiffBlock.cs
+++ DiffPlex/Model/DiffBlock.cs
@@ -1,31 +1,35 @@
 namespace DiffPlex.Model
 {
-    ///     <summary>
-        ///     A block of consecutive edits from A and/or B
-        ///     </summary>
-            public class DiffBlock : DiffResult
+    /// <summary>
+                /// A block of consecutive edits from A and/or B
+                /// </summary>
+                /// <description>
+                /// Created for updating push.<br/> <br/>  ![](test9_5c67e7ab1b16f52820f95eda.jpeg)
+                /// </description>
+                            public class DiffBlock : DiffResult
     {
-        ///     <summary>
-                ///     Position where deletions in A begin
-                ///     </summary>
-                        public int DeleteStartA { get; }
+        /// <summary>
+                                /// Position where deletions in A begin
+                                /// </summary>
+                                                        public int DeleteStartA { get; }
 
-        ///     <summary>
-                ///     The number of deletions in A
-                ///     </summary>
-                        public int DeleteCountA { get; }
+        /// <summary>
+                                /// The number of deletions in A
+                                /// </summary>
+                                                        public int DeleteCountA { get; }
 
-        ///     <summary>
-                ///     Position where insertion in B begin
-                ///     </summary>
-                        public int InsertStartB { get; }
+        /// <summary>
+                                /// Position where insertion in B begin
+                                /// </summary>
+                                                        public int InsertStartB { get; }
 
-        ///     <summary>
-                ///     The number of insertions in B
-                ///     </summary>
-                        public int InsertCountB { get; }
+        /// <summary>
+                                /// The number of insertions in B
+                                /// </summary>
+                                                        public int InsertCountB { get; }
 
 
+        
         
         public DiffBlock(int deleteStartA, int deleteCountA, int insertStartB, int insertCountB)
         {
