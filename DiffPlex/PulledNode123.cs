8168a9998e1f0c475c24feacb434d229d5928672
unimo <35590162+unitydemo2@users.noreply.github.com>
2019-02-18 18:55:43 +0530

    Create PulledNode123.cs

---
+++ DiffPlex/PulledNode123.cs
@@ -0,0 +1,20 @@
+//Interface for the Merge Algorithm implementation class - Mohit Sahu
+
+using DiffPlex.DiffBuilder.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiffPlex
+{
+    
+    public interface PulledNode123
+    {
+        
+        SideBySideDiffModel GetMergedText(List<string> list);
+
+        
+        SideBySideDiffModel GetMergedText(SideBySideDiffModel model);
+    }
+}
