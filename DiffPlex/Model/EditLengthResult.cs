af8dc5b800f59b85cb8cef6eb9b84eec975e9f37
CMSQATeam <cmsqa123@gmail.com>
2019-02-19 12:39:05 +0530

    DW CMS Message

--- DiffPlex/Model/EditLengthResult.cs
+++ DiffPlex/Model/EditLengthResult.cs
@@ -17,35 +17,40 @@ namespace DiffPlex.Model
 
     
     /// <summary>
-    /// This is created for testing
-    /// </summary>
-    /// <description>
-    /// ![abc](301805_5c6b9b40c6f96208d4f086d7.jpg)
-    /// </description>
-    public class EditLengthResult
+        /// This is created for testing
+        /// </summary>
+        /// <description>
+        /// ![abc](301805_5c6b9b40c6f96208d4f086d7.jpg)
+        /// </description>
+            public class EditLengthResult
     {
         
         /// <summary>
-        /// [5c6ba7006cab41050403cd4e](childnodesinheritflas_5c6ba7006cab41050403cd4e.cs)
-        /// </summary>
-        /// <description>
-        /// []()
-        /// </description>
-        public int EditLength { get; set; }
+                /// [5c6ba89d6cab41050403d54d](TG1_5c6ba89d6cab41050403d54d.cs)
+                /// </summary>
+                /// <description>
+                /// []()
+                /// </description>
+                        public int EditLength { get; set; }
 
+        
         
         
         public int StartX { get; set; }
         
         
+        
         public int EndX { get; set; }
+        
         
         
         public int StartY { get; set; }
         
         
+        
         public int EndY { get; set; }
 
+        
         
         
         public Edit LastEdit { get; set; }
---
+++ QAImages/Examples/TG1_5c6ba89d6cab41050403d54d.cs
@@ -0,0 +1,85 @@
+{
+   "content":    {
+      "tagGroupName": "CMS-API:VBXAX",
+      "limitToOne": false,
+      "childNodesInherit": false,
+      "displayGroupName": true,
+      "public": true,
+      "colour": "Blue",
+      "publish": false,
+      "isSystemManaged": false,
+      "inputType": 1,
+      "tagGroupId": "5a72bfd942f30114c08c0a3b"
+   },
+   "userId": "S-1-5-21-1210654208-2246142303-2829877410-19101",
+   "flowMap":    {
+      "cmsOperation": 26,
+      "eventGroups": [[      {
+         "eventName": 1034,
+         "eventLocationInFlowMap": null,
+         "status": 2,
+         "childCMSOperation": 0,
+         "childOperationStatus": null,
+         "eventCondition": null,
+         "reuseCmsOperation": 0,
+         "conditionKeys": null,
+         "requestMapProperty":          [
+                        {
+               "source": "TagGroupName",
+               "target": "TagGroupName"
+            },
+                        {
+               "source": "LimitToOne",
+               "target": "LimitToOne"
+            },
+                        {
+               "source": "ChildNodesInherit",
+               "target": "ChildNodesInherit"
+            },
+                        {
+               "source": "DisplayGroupName",
+               "target": "DisplayGroupName"
+            },
+                        {
+               "source": "Public",
+               "target": "Public"
+            },
+                        {
+               "source": "Colour",
+               "target": "Colour"
+            },
+                        {
+               "source": "Publish",
+               "target": "Publish"
+            },
+                        {
+               "source": "IsSystemManaged",
+               "target": "IsSystemManaged"
+            },
+                        {
+               "source": "InputType",
+               "target": "InputType"
+            }
+         ],
+         "responseMapProperty": [         {
+            "source": "TagGroupId",
+            "target": "TagGroupId"
+         }]
+      }]],
+      "notificationType": 2,
+      "notificationTopic": "TagGroup",
+      "parentResponseId": null,
+      "parentEventLocation": null,
+      "isOperationCompleteWithStatusSuccess": true,
+      "_id": "5a72a5e44af10f8978deb4df",
+      "status": 0,
+      "modifiedDate": 0,
+      "createdDate": 1517463012
+   },
+   "createdOn": "2018-02-01T07:20:57.174Z",
+   "errorResponse": null,
+   "_id": "5a72bfd916850626504a8e1b",
+   "status": 2,
+   "modifiedDate": 1517469657,
+   "createdDate": 1517469657
+}
