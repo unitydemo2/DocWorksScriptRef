c12761449d03eae908beee0ecfc6ab293e1d2893
CMSQATeam <cmsqa123@gmail.com>
2019-02-21 11:45:58 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/ClassProperty.cs
+++ DiffPlex/DiffBuilder/Model/ClassProperty.cs
@@ -2,13 +2,14 @@ namespace DiffPlex.DiffBuilder.Model
 {
     
     /// <summary>
-                                                                /// Editor API for the Unity Services editor feature. Normally UnityAds is enabled from the Services window, but if writing your own editor extension, this API can be used.
-                                                                /// </summary>
-                                                                /// <description>
-                                                                /// [5c6be5da6cab4105040479da](hello_5c6be5da6cab4105040479da.cs)<br/> <br/>
-                                                                /// </description>
-                                                                                                                            public class ClassProperty
+                                                                                                                                /// Editor API for the Unity Services editor feature. Normally UnityAds is enabled from the Services window, but if writing your own editor extension, this API can be used.
+                                                                                                                                /// </summary>
+                                                                                                                                /// <description>
+                                                                                                                                /// ![abc](angeler_5c6b9b40c6f96208d4f086d6.jpg)
+                                                                                                                                /// </description>
+                                                                                                                                                                                                                                                            public class ClassProperty
     {
+        
         
         
         
@@ -23,8 +24,10 @@ namespace DiffPlex.DiffBuilder.Model
         
         
         
+        
         public int NumberB { get; set; }
 
+        
         
         
         
