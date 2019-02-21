7fa95f769f3ca253e2094d4470f289e73b58b94d
CMSQATeam <cmsqa123@gmail.com>
2019-02-21 18:23:38 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
+++ DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 namespace DiffPlex.DiffBuilder.Model
 {
     
+    /// <summary>
+    /// The BurstDiscard attribute lets you remove a method or property from being compiled to native code by the burst compiler.
+    /// </summary>
+    /// <description>
+    /// [5c6be5da6cab4105040479da](hello_5c6be5da6cab4105040479da.cs)
+    /// </description>
     public class DiffPaneModel
     {
         
+        
         public List<DiffPiece> Lines { get; }
 
+        
         
         public DiffPaneModel()
         {
