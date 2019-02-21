63785e93cf0f6e9f0bdd29c862b449b1d2f811de
CMSQATeam <cmsqa123@gmail.com>
2019-02-21 10:36:01 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/ClassProperty.cs
+++ DiffPlex/DiffBuilder/Model/ClassProperty.cs
@@ -2,21 +2,24 @@ namespace DiffPlex.DiffBuilder.Model
 {
     
     /// <summary>
-    /// Editor API for the Unity Services editor feature. Normally UnityAds is enabled from the Services window, but if writing your own editor extension, this API can be used.
-    /// </summary>
-    /// <description>
-    /// [5c6beeef6cab410504049596](2D01_5c6beeef6cab410504049596.cs)
-    /// </description>
-    public class ClassProperty
+        /// Editor API for the Unity Services editor feature. Normally UnityAds is enabled from the Services window, but if writing your own editor extension, this API can be used.
+        /// </summary>
+        /// <description>
+        /// ![abc](angeler_5c6b9b40c6f96208d4f086d6.jpg)
+        /// </description>
+            public class ClassProperty
     {
+        
         
         
         public int NumberA { get; set; }
 
         
         
+        
         public int NumberB { get; set; }
 
+        
         
         
         public int NumberC { get; set; }
