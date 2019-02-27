f304e2d0825cdc4cad5953086109066882f883b2
CMSQATeam <cmsqa123@gmail.com>
2019-02-27 15:49:11 +0530

    DW CMS Message

--- DiffPlex/Log.cs
+++ DiffPlex/Log.cs
@@ -1,22 +1,26 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace DiffPlex
 {
+    
     public static class Log
     {
+        
         [Conditional("LOG")]
         public static void WriteLine(string format, params object[] args)
         {
             Debug.WriteLine(string.Format(format, args));
         }
 
+        
         [Conditional("LOG")]
         public static void Write(string format, params object[] args)
         {
             // not implemented
         }
 
+        
         public static List<string> NumberList()
         {
             return new List<string>() { "1", "2", "3" };
