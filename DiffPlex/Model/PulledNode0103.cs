ce7132929983bb7a8241bc0e750e6ce33c230d50
unimo <35590162+unitydemo2@users.noreply.github.com>
2019-03-01 10:17:56 +0530

    Create PulledNode0103.cs

---
+++ DiffPlex/Model/PulledNode0103.cs
@@ -0,0 +1,27 @@
+namespace DiffPlex.Model
+{
+    
+    public class PulledNode0103
+    {
+        
+        public int[] HashedPieces { get; set; }
+
+        
+        public string RawData { get; }
+
+        
+        public bool[] Modifications { get; set; }
+
+        
+        public string[] Pieces { get; set; }
+
+        public delegate int TestDelegate1221(string s);
+
+        public delegate void CalculateSquare1221(double number);
+        
+        public PulledNode0103(string str)
+        {
+            RawData = str;
+        }
+    }
+}
