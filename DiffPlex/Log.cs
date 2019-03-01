a11b208fc394ff071dda122fc3c7b146a7489ee7
CMSQATeam <cmsqa123@gmail.com>
2019-03-01 15:51:24 +0530

    DW CMS Message

--- DiffPlex/Log.cs
+++ DiffPlex/Log.cs
@@ -3,20 +3,24 @@ using System.Diagnostics;
 
 namespace DiffPlex
 {
+    
     public static class Log
     {
+        
         [Conditional("LOG")]
         public static void WriteLine(string format, params object[] args)
         {
             Debug.WriteLine(string.Format(format, args));
         }
 
+        
         [Conditional("LOG")]
         public static void Write(string format, params object[] args)
         {
             // not implemented
         }
 
+        
         public static List<string> NumberList()
         {
             return new List<string>() { "1", "2", "3" };
