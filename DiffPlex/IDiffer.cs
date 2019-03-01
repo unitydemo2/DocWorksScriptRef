7e20127c69accc2774a051ed5bdd8a4f0907da43
CMSQATeam <cmsqa123@gmail.com>
2019-03-01 14:48:46 +0530

    DW CMS Message

--- DiffPlex/IDiffer.cs
+++ DiffPlex/IDiffer.cs
@@ -3,25 +3,44 @@ using DiffPlex.Model;
 
 namespace DiffPlex
 {
-    ///     <summary>
-        ///     Provides methods for generate differences between texts
-        ///     </summary>
-            public interface IDiffer
+    /// <summary>
+                /// Provides methods for generate differences between texts
+                /// </summary>
+                /// <description>
+                /// Some description for differ
+                /// </description>
+                            public interface IDiffer
     {
+        
         
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace);
         
+        
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
         
+        /// <summary>
+        /// Summary for charDiff method
+        /// </summary>
+        /// <description>
+        /// Well i am <br/>  Description for my method
+        /// </description>
+        /// <returns>
+        /// Return desc details..
+        /// </returns>
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace);
+        
         
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
         
+        
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, char[] separators);
+        
         
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase, char[] separators);
         
+        
         DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, Func<string, string[]> chunker);
+        
         
         DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, bool ignoreCase, Func<string, string[]> chunker);
     }
