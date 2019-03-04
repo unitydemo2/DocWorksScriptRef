bf487bba25f14447b1abffebd1d2664cdd74861e
CMSQATeam <cmsqa123@gmail.com>
2019-03-04 16:08:07 +0530

    DW CMS Message

--- DiffPlex/AloksSummaryTest.cs
+++ DiffPlex/AloksSummaryTest.cs
@@ -5,17 +5,19 @@ namespace DiffPlex
     
     
     
+    
     [Obsolete]
     public class AloksSummaryTest
     {
         private static readonly string[] emptyStringArray = new string[0];
-        ///     <summary>
-        ///     this is the property for 
-        ///     ![Enfield image](Enfiled.jpg)
-        ///     </summary>
-        [Obsolete]
+        ///         <summary>
+                ///         this is the property for 
+                ///         ![Enfield image](Enfiled.jpg)
+                ///         </summary>
+                        [Obsolete]
         public string obsoletedPropertyForTesting { get; set; }
 
+        
         
         
         
@@ -27,14 +29,14 @@ namespace DiffPlex
                 return "not equal";
         }
 
-        ///     <summary>
-                                                                ///     this method is to find diffs in 2 texts
-                                                                ///     </summary>
-                                                                ///     <param name="oldText">this param is for existing text</param>
-                                                                ///     <param name="newText">this param is for updated text</param>
-                                                                ///     <param name="ignoreWhitespace"></param>
-                                                                ///     <param name="ignoreCase"></param>
-                                                                                                                        [Obsolete]
+        ///         <summary>
+                                                                                                                                ///         this method is to find diffs in 2 texts
+                                                                                                                                ///         </summary>
+                                                                                                                                ///         <param name="oldText">this param is for existing text</param>
+                                                                                                                                ///         <param name="newText">this param is for updated text</param>
+                                                                                                                                ///         <param name="ignoreWhitespace"></param>
+                                                                                                                                ///         <param name="ignoreCase"></param>
+                                                                                                                                                                                                                                                        [Obsolete]
         public bool CompareStringLength(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase)
         {
             if (oldText == null) throw new ArgumentNullException(nameof(oldText));
@@ -42,16 +44,16 @@ namespace DiffPlex
             return oldText.Length > newText.Length;
         }
 
-        ///     <summary>
-                                                                ///     This is a test description for this method
-                                                                ///     </summary>
-                                                                ///     <param name="oldText">Old text description</param>
-                                                                ///     <param name="newText">new text description</param>
-                                                                ///     <param name="ignoreWhitespace">Should ignore blank space or not</param>
-                                                                ///     <returns>
-                                                                ///     Method returns a string
-                                                                ///     </returns>
-                                                                                                                        public string CreateSomeOutput(string oldText, string newText, bool ignoreWhitespace)
+        ///         <summary>
+                                                                                                                                ///         This is a test description for this method
+                                                                                                                                ///         </summary>
+                                                                                                                                ///         <param name="oldText">Old text description</param>
+                                                                                                                                ///         <param name="newText">new text description</param>
+                                                                                                                                ///         <param name="ignoreWhitespace">Should ignore blank space or not</param>
+                                                                                                                                ///         <returns>
+                                                                                                                                ///         Method returns a string
+                                                                                                                                ///         </returns>
+                                                                                                                                                                                                                                                        public string CreateSomeOutput(string oldText, string newText, bool ignoreWhitespace)
         {
             return "Test Output";
         }
