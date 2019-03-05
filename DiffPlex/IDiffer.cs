e83934f35d4ade3e8507ef236e4aa02d38396d0f
CMSQATeam <cmsqa123@gmail.com>
2019-03-05 12:32:12 +0530

    DW CMS Message

--- DiffPlex/IDiffer.cs
+++ DiffPlex/IDiffer.cs
@@ -3,25 +3,36 @@ using DiffPlex.Model;
 
 namespace DiffPlex
 {
-    ///     <summary>
-        ///     Provides methods for generate differences between texts
-        ///     </summary>
-            public interface IDiffer
+    /// <summary>
+                /// Provides methods for generate differences between texts
+                /// </summary>
+                /// <description>
+                /// ![](waterbird_5c74fc03bf9f0a0194bb1e30.jpeg)
+                /// </description>
+                            public interface IDiffer
     {
+        
         
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace);
         
+        
         DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
+        
         
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace);
         
+        
         DiffResult CreateCharacterDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase);
+        
         
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, char[] separators);
         
+        
         DiffResult CreateWordDiffs(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase, char[] separators);
         
+        
         DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, Func<string, string[]> chunker);
+        
         
         DiffResult CreateCustomDiffs(string oldText, string newText, bool ignoreWhiteSpace, bool ignoreCase, Func<string, string[]> chunker);
     }
