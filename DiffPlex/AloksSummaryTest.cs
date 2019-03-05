cc6e7e29940c8d58972d2065d7bf6b3708b3e7bf
CMSQATeam <cmsqa123@gmail.com>
2019-03-05 12:32:26 +0530

    DW CMS Message

--- DiffPlex/AloksSummaryTest.cs
+++ DiffPlex/AloksSummaryTest.cs
@@ -3,17 +3,23 @@ using System;
 namespace DiffPlex
 {
     
+    /// <description>
+    /// ![Tiger eyes image](Eyes_5c7d098562472f50dc7bac49.jpg)
+    /// </description>
     [Obsolete]
     public class AloksSummaryTest
     {
         private static readonly string[] emptyStringArray = new string[0];
-        ///     <summary>
-                ///     this is the property for 
-                ///     </summary>
-                        [Obsolete]
+        /// <summary>
+                                /// this is the property for
+                                /// </summary>
+                                                        [Obsolete]
         public string obsoletedPropertyForTesting { get; set; }
 
         
+        /// <summary>
+        /// ![abc](DW5a963922d2f2b83b4ce3e9c6_5c669d117f229462684627ac.png)
+        /// </summary>
         public string CompareString(string oldText, string newText, bool ignoreWhitespace)
         {
             if (oldText.Equals(newText))
@@ -22,14 +28,10 @@ namespace DiffPlex
                 return "not equal";
         }
 
-        ///     <summary>
-                ///     this method is to find diffs in 2 texts
-                ///     </summary>
-                ///     <param name="oldText">this param is for existing text</param>
-                ///     <param name="newText">this param is for updated text</param>
-                ///     <param name="ignoreWhitespace"></param>
-                ///     <param name="ignoreCase"></param>
-                        [Obsolete]
+        /// <summary>
+                                /// this method is to find diffs in 2 texts
+                                /// </summary>
+                                                        [Obsolete]
         public bool CompareStringLength(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase)
         {
             if (oldText == null) throw new ArgumentNullException(nameof(oldText));
@@ -37,16 +39,13 @@ namespace DiffPlex
             return oldText.Length > newText.Length;
         }
 
-        ///     <summary>
-                ///     This is a test description for this method
-                ///     </summary>
-                ///     <param name="oldText">Old text description</param>
-                ///     <param name="newText">new text description</param>
-                ///     <param name="ignoreWhitespace">Should ignore blank space or not</param>
-                ///     <returns>
-                ///     Method returns a string
-                ///     </returns>
-                        public string CreateSomeOutput(string oldText, string newText, bool ignoreWhitespace)
+        /// <summary>
+                                /// This is a test description for this method
+                                /// </summary>
+                                /// <returns>
+                                /// Method returns a string
+                                /// </returns>
+                                                        public string CreateSomeOutput(string oldText, string newText, bool ignoreWhitespace)
         {
             return "Test Output";
         }
