4b419f27473351aecc0ec606aea746505bd13994
CMSQATeam <cmsqa123@gmail.com>
2019-03-06 16:59:56 +0530

    DW CMS Message

--- DiffPlex/Merger.cs
+++ DiffPlex/Merger.cs
@@ -6,16 +6,16 @@ using DiffPlex.DiffBuilder.Model;
 
 namespace DiffPlex
 {
-    ///         <summary>
-                ///         this is the class for meging 2 texts
-                ///         testing for pull by sirisha 1234567890-
-                ///         ![abc](codetest.cs)  testing for code blocks   
-                ///         </summary>
-                ///         <description>
-                ///         ![abc](birdsfly.jpg) testing for pull
-                ///         merging left and right texts and getting output as merged text
-                ///         </description>
-                            public class Merger : IMerger
+    ///             <summary>
+                                ///             this is the class for meging 2 texts
+                                ///             testing for pull by sirisha 1234567890-
+                                ///             ![abc](codetest.cs)  testing for code blocks   
+                                ///             </summary>
+                                ///             <description>
+                                ///             ![abc](birdsfly.jpg) testing for pull
+                                ///             merging left and right texts and getting output as merged text
+                                ///             </description>
+                                                            public class Merger : IMerger
     {
         ///     <summary>
                 ///     enumerator for Merger
@@ -33,48 +33,48 @@ namespace DiffPlex
             MergedText
         }
  
-        ///         <summary>
-                                ///         summary for constructor pull test commit test
-                                ///         </summary>
-                                                        public Merger()
+        ///             <summary>
+                                                                ///             summary for constructor pull test commit test
+                                                                ///             </summary>
+                                                                                                                        public Merger()
         {
             Console.WriteLine("This is demo constructor");
         }
 
-        ///         <summary>
-                                ///         FieldExample1
-                                ///         </summary>
-                                ///         <description>
-                                ///         This is for generating Field example 1
-                                ///         </description>
-                                                        public int FieldExample1;
+        ///             <summary>
+                                                                ///             FieldExample1
+                                                                ///             </summary>
+                                                                ///             <description>
+                                                                ///             This is for generating Field example 1
+                                                                ///             </description>
+                                                                                                                        public int FieldExample1;
 
-        ///         <summary>
-                                ///         FieldExample2
-                                ///         </summary>
-                                ///         <description>
-                                ///         This is for generating Field example 2
-                                ///         </description>
-                                                        public string FieldExample2;
+        ///             <summary>
+                                                                ///             FieldExample2
+                                                                ///             </summary>
+                                                                ///             <description>
+                                                                ///             This is for generating Field example 2
+                                                                ///             </description>
+                                                                                                                        public string FieldExample2;
 
-        ///         <summary>
-                                ///         Property 1
-                                ///         </summary>
-                                ///         <description>
-                                ///         This is for generating Property 1
-                                ///         </description>
-                                                        public string Property1 { get; set; }
+        ///             <summary>
+                                                                ///             Property 1
+                                                                ///             </summary>
+                                                                ///             <description>
+                                                                ///             This is for generating Property 1
+                                                                ///             </description>
+                                                                                                                        public string Property1 { get; set; }
 
-        ///         <summary>
-                                ///         Property 2
-                                ///         </summary>
-                                ///         <description>
-                                ///         This is for generating Property 2
-                                ///         </description>
-                                                        public long Property2 { get; set; }
+        ///             <summary>
+                                                                ///             Property 2
+                                                                ///             </summary>
+                                                                ///             <description>
+                                                                ///             This is for generating Property 2
+                                                                ///             </description>
+                                                                                                                        public long Property2 { get; set; }
 
-        ///     <!-- Badly formed XML comment ignored for member "M:DiffPlex.Merger.GetMergedText(DiffPlex.DiffBuilder.Model.SideBySideDiffModel,System.String)" -->
-                                                        public static SideBySideDiffModel GetMergedText(SideBySideDiffModel model, string model2)
+        ///         <!-- Badly formed XML comment ignored for member "M:DiffPlex.Merger.GetMergedText(DiffPlex.DiffBuilder.Model.SideBySideDiffModel,System.String)" -->
+                                                                                                                        public static SideBySideDiffModel GetMergedText(SideBySideDiffModel model, string model2)
         {
             DiffPaneModel mergedText = new DiffPaneModel();
 
@@ -116,12 +116,14 @@ namespace DiffPlex
 
         
         
+        
         public SideBySideDiffModel GetMergedText(List<string> list)
         {
             //throw new NotImplementedException();
             return new SideBySideDiffModel();
         }
 
+        
         
         
         public SideBySideDiffModel GetMergedText(SideBySideDiffModel model)
