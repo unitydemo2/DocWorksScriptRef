8e23bf216f848932644ff82a46949d6827341c75
CMSQATeam <cmsqa123@gmail.com>
2019-03-07 12:56:16 +0530

    DW CMS Message

--- DiffPlex/Log.cs
+++ DiffPlex/Log.cs
@@ -4,8 +4,12 @@ using System.Diagnostics;
 namespace DiffPlex
 {
     
+    /// <description>
+    /// ![](The-power-of-unity_5c74fb73bf9f0a0194bb1e0d.png)
+    /// </description>
     public static class Log
     {
+        
         
         [Conditional("LOG")]
         public static void WriteLine(string format, params object[] args)
@@ -14,6 +18,7 @@ namespace DiffPlex
         }
 
         
+        
         [Conditional("LOG")]
         public static void Write(string format, params object[] args)
         {
@@ -21,6 +26,9 @@ namespace DiffPlex
         }
 
         
+        /// <summary>
+        /// Tesing pull operation688790
+        /// </summary>
         public static List<string> NumberList()
         {
             return new List<string>() { "1", "2", "3" };
