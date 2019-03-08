17fd74e551da694880f1795301f77d0b8758c753
adaftuar-rythmos <45761202+adaftuar-rythmos@users.noreply.github.com>
2019-03-08 16:49:07 +0530

    Create CrashReportingSettings.bindings.cs

---
+++ DiffPlex/FailedFiles/CrashReportingSettings.bindings.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.Bindings;
+
+namespace UnityEditor.CrashReporting
+{
+#if ENABLE_CLOUD_SERVICES_CRASH_REPORTING
+
+    [NativeHeader("Modules/UnityConnect/CrashReporting/CrashReportingSettings.h")]
+    [StaticAccessor("GetCrashReportingSettings()")]
+    public static partial class CrashReportingSettings
+    {
+        public static extern bool enabled
+        {
+            [ThreadSafe] get;
+            [ThreadSafe] set;
+        }
+
+        public static extern bool captureEditorExceptions { get; set; }
+
+        [NativeThrows]
+        public static extern UInt32 logBufferSize { get; set; }
+
+        internal static extern void SetEnabledServiceWindow(bool enabled);
+
+        internal static extern string GetEventUrl();
+        internal static extern void SetEventUrl(string eventUrl);
+    }
+
+#endif
+}
