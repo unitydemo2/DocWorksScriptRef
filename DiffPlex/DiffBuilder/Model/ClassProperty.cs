a97670801904bd579003960dcbdf621ea153afb2
CMSQATeam <cmsqa123@gmail.com>
2019-03-11 11:01:58 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/ClassProperty.cs
+++ DiffPlex/DiffBuilder/Model/ClassProperty.cs
@@ -1,14 +1,33 @@
 namespace DiffPlex.DiffBuilder.Model
 {
     
+    /// <summary>
+    /// This is updated for testing the push operation.
+    /// </summary>
+    /// <description>
+    /// ![](Fish_5c7509e9bf9f0a0194bc06fb.jpeg)
+    /// </description>
     public class ClassProperty
     {
         
+        /// <summary>
+        /// [5c754060bf9f0a0194bc200a](escape_5c754060bf9f0a0194bc200a.cs)
+        /// </summary>
+        /// <description>
+        /// This is updated for testing the push operation.
+        /// </description>
         public int NumberA { get; set; }
 
         
+        /// <summary>
+        /// This is the cms link created for testing the push operation.
+        /// </summary>
+        /// <description>
+        /// []()
+        /// </description>
         public int NumberB { get; set; }
 
+        
         
         public int NumberC { get; set; }
     }
