950317a7f64d5d4d544a6ac9c9f2180046a67ed2
CMSQATeam <cmsqa123@gmail.com>
2019-03-11 12:02:04 +0530

    DW CMS Message

--- DiffPlex/DiffBuilder/Model/PulledNode.cs
+++ DiffPlex/DiffBuilder/Model/PulledNode.cs
@@ -2,18 +2,20 @@ using System.Collections.Generic;
 
 namespace DiffPlex.DiffBuilder.Model
 {
-     /// <summary>
-        ///  This is created for tetsing the summary.
-        /// ![Image](Fish_5c7509e9bf9f0a0194bc06fb.jpeg)
-        /// ![Code](escape_5c754060bf9f0a0194bc200a.cs)
-        /// Assets Added here for pull testing.
-        ///</summary>   
-        
+     ///      <summary>
+    ///       This is created for tetsing the summary.
+    ///      ![Image](Fish_5c7509e9bf9f0a0194bc06fb.jpeg)
+    ///      ![Code](escape_5c754060bf9f0a0194bc200a.cs)
+    ///      Assets Added here for pull testing.
+    ///     </summary>   
+            
     public class PulledNode
     {
         
+        
         public List<DiffPiece> Lines { get; }
 
+        
         
         public PulledNode()
         {
