6ae5f80cedd7462c7b4aa530707cb148ce6d4247
unimo <35590162+unitydemo2@users.noreply.github.com>
2019-03-11 11:57:54 +0530

    Update DiffPaneModel.cs

--- DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
+++ DiffPlex/DiffBuilder/Model/DiffPaneModel.cs
@@ -2,7 +2,12 @@ using System.Collections.Generic;
 
 namespace DiffPlex.DiffBuilder.Model
 {
-    
+        /// <summary>
+        ///  This is created for tetsing the summary.
+        /// ![Image](Fish_5c7509e9bf9f0a0194bc06fb.jpeg)
+        /// ![Code](escape_5c754060bf9f0a0194bc200a.cs)
+        /// Assets Added here for pull testing.
+        ///</summary>   
     public class DiffPaneModel
     {
         
