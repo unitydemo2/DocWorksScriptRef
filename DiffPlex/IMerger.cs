c6cde34d9536e8ac7934a5d3922346bc5d2e85c2
CMSQATeam <cmsqa123@gmail.com>
2019-03-11 12:23:25 +0530

    DW CMS Message

--- DiffPlex/IMerger.cs
+++ DiffPlex/IMerger.cs
@@ -9,27 +9,36 @@ using System.Text;
 namespace DiffPlex
 {
     /// <summary>
-        /// ```
-        ///  Class ABC {
-        ///  Int a;
-        ///  Int b;
-        ///  }
-        /// ```
-        /// </summary>
-        /// <description>
-        /// ```
-        ///  Class ABC {
-        ///  Int a;
-        ///  Int b;
-        ///  }
-        /// ```
-        /// </description>
-            public interface IMerger
+                /// ```
+                ///  Class ABC {
+                ///  Int a;
+                ///  Int b;
+                ///  }
+                /// ```
+                /// <br/>  ![](The-power-of-unity_5c74fb73bf9f0a0194bb1e0d.png)<br/>  [5c750355bf9f0a0194bb92fc](adding_5c750355bf9f0a0194bb92fc.cs)<br/> <br/>  
+                /// ```
+                ///  Class ABC {
+                ///  Int a;
+                ///  Int b;
+                ///  }
+                /// ```
+                /// </summary>
+                /// <description>
+                /// ```
+                ///  Class ABC {
+                ///  Int a;
+                ///  Int b;
+                ///  }
+                /// ```
+                /// </description>
+                            public interface IMerger
     {
+        
         
         
         SideBySideDiffModel GetMergedText(List<string> list);
 
+        
         
         
         SideBySideDiffModel GetMergedText(SideBySideDiffModel model);
