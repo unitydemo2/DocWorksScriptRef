8dbcf573c08882507af41dd4b5bd061163711a17
adaftuar-rythmos <45761202+adaftuar-rythmos@users.noreply.github.com>
2019-03-15 16:51:27 +0530

    Create DifferNew.cs

---
+++ DiffPlex/DifferNew.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using DiffPlex.Model;
+
+namespace DiffPlex
+{
+    
+   
+    public class Differ : IDiffer
+    {
+        private static readonly string[] emptyStringArray = new string[0];
+
+        ///     <summary>
+                ///     this is the property for 
+                ///     </summary>
+                        
+        public string PropertyForTesting { get; set; }
+
+        }
+ }
