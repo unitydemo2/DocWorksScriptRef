e4fd964ef93ba2e2d945fc4bffd6727739925ce1
CMSQATeam <cmsqa123@gmail.com>
2019-03-18 13:39:55 +0530

    DW CMS Message SortedMethods.cs

--- DiffPlex/SortedMethods.cs
+++ DiffPlex/SortedMethods.cs
@@ -1,22 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DiffPlex
 {
+    
     public class SortedMethods
     {
+        
         public void CopyFrom(int[] array)
         {
 
         }
 
-        /// <summary>
-        /// This method will sort list in given order 
-        /// </summary>
-        /// <param name="array">list to be sorted</param>
-        /// <param name="sortInAscOrder">Order of sorting.</param>
-        public List<int> SortList(List<int> array, bool sortInAscOrder)
+        ///     <summary>
+                ///     This method will sort list in given order 
+                ///     </summary>
+                ///     <param name="array">list to be sorted</param>
+                ///     <param name="sortInAscOrder">Order of sorting.</param>
+                        public List<int> SortList(List<int> array, bool sortInAscOrder)
         {
             if (sortInAscOrder)
             {
@@ -29,6 +31,7 @@ namespace DiffPlex
             return array;
         }
 
+        
         public int[] SliceWithStride(int[] input)
         {
             return new int[0];
