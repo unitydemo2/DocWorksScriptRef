49905804e3cd80de7c721c41da0220dfad866b3c
adaftuar-rythmos <45761202+adaftuar-rythmos@users.noreply.github.com>
2019-03-19 11:49:09 +0530

    Create TestClass.cs

---
+++ DiffPlex/TestClass.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiffPlex
+{
+    
+    public class TestClass
+    {
+        /// <summary>
+        /// This method will sort list in given order 
+        /// </summary>
+        /// <param name="array">list to be sorted</param>
+        /// <param name="sortInAscOrder">Order of sorting.</param>
+        public List<int> SortList(List<int> array, bool sortInAscOrder)
+        {
+            if (sortInAscOrder)
+            {
+                array.Sort();
+            }
+            else
+            {
+                array.Sort();
+                array.Reverse();
+            }
+            return array;
+        }
+
+        
+        public void Add()
+        {
+            Console.Write("You called add method");
+        }
+
+        
+        public void Multiply(int a, int b)
+        {
+            Console.Write(x * y);
+        }
+
+        ///     <summary>
+        ///     This is a test method
+        ///     </summary>
+        ///     <param name="x"></param>
+        ///     <param name="y"></param>
+        public void Divide(int x, int y) {
+            Console.Write(x / y);
+        }
+    }
+}
