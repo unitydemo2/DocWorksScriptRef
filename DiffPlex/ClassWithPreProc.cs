b6d1c032656b2f5be343480d491c90f59e052c6a
adaftuar-rythmos <45761202+adaftuar-rythmos@users.noreply.github.com>
2019-03-19 17:06:45 +0530

    Create ClassWithPreProc.cs

---
+++ DiffPlex/ClassWithPreProc.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace DiffPlex
+{
+
+#if ENABLE_CLOUD_PROJECT_ID
+
+#endif
+    
+    public class ClassWithPreProc
+    {
+        private static readonly string[] emptyStringArray = new string[0];
+        /// <summary>
+        /// this is the property for 
+        ///     ![Enfield image](Enfiled.jpg)
+        /// </summary>
+        
+        public string PropertyForTesting { get; set; }
+
+        
+        
+        public string CreateSomeOutput(string oldText, string newText, bool ignoreWhitespace)
+        {
+            return "Test Output";
+        }
+    }
+}
