76cf4015713cac0cdb8f588d1ddf36adcd432d0e
CMSQATeam <cmsqa123@gmail.com>
2019-03-19 15:24:51 +0530

    DW CMS Message C://CMSProject/5c90b92ade8e591d103fb2f6/Assets/Images/beauty_5c7e227def98045e009a9b7a.jpg

--- DiffPlex/FunnyClass.cs
+++ DiffPlex/FunnyClass.cs
@@ -5,14 +5,18 @@ using System.Text;
 namespace DiffPlex
 {
     
+    /// <summary>
+    /// I am a funny class.
+    /// </summary>
+    /// <description>
+    /// Really funny class, very very funny.<br/>  ![Flower image](beauty_5c7e227def98045e009a9b7a.jpg)
+    /// </description>
     public class FunnyClass
     {
         /// <summary>
-        /// This method will sort list in given order 
-        /// </summary>
-        /// <param name="array">list to be sorted</param>
-        /// <param name="sortInAscOrder">Order of sorting.</param>
-        public List<int> SortList(List<int> array, bool sortInAscOrder)
+                /// This method will sort list in given order
+                /// </summary>
+                        public List<int> SortList(List<int> array, bool sortInAscOrder)
         {
             if (sortInAscOrder)
             {
@@ -26,6 +30,7 @@ namespace DiffPlex
             return array;
         }
 
+        
         
         public void Add()
         {
