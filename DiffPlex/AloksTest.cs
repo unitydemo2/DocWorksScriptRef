bade41e25fef8cda687fd405f4aee0bf509b41fb
CMSQATeam <cmsqa123@gmail.com>
2019-03-19 13:02:44 +0530

    DW CMS Message

--- DiffPlex/AloksTest.cs
+++ DiffPlex/AloksTest.cs
@@ -8,16 +8,17 @@ namespace DiffPlex
 #if ENABLE_CLOUD_PROJECT_ID
 #endif
     
+    
     public class AloksTest
     {
         private static readonly string[] emptyStringArray = new string[0];
-        ///     <summary>
-                ///     this is the property for 
-                ///     ![Enfield image](Enfiled.jpg)
-                ///     </summary>
-                        [Obsolete]
+        /// <summary>
+                                /// this is the property for ![Enfield image](Enfiled_5c7e2d1851c5cf115852cded.jpg)
+                                /// </summary>
+                                                        [Obsolete]
         public string obsoletedPropertyForTesting { get; set; }
 
+        
         
         public string CompareString(string oldText, string newText, bool ignoreWhitespace)
         {
@@ -27,14 +28,10 @@ namespace DiffPlex
                 return "not equal";
         }
 
-        ///     <summary>
-                ///     this method is to find diffs in 2 texts
-                ///     </summary>
-                ///     <param name="oldText">this param is for existing text</param>
-                ///     <param name="newText">this param is for updated text</param>
-                ///     <param name="ignoreWhitespace"></param>
-                ///     <param name="ignoreCase"></param>
-                        [Obsolete]
+        /// <summary>
+                                /// this method is to find diffs in 2 texts
+                                /// </summary>
+                                                        [Obsolete]
         public bool CompareStringLength(string oldText, string newText, bool ignoreWhitespace, bool ignoreCase)
         {
             if (oldText == null) throw new ArgumentNullException(nameof(oldText));
@@ -42,6 +39,7 @@ namespace DiffPlex
             return oldText.Length > newText.Length;
         }
 
+        
         
         public string CreateSomeOutput(string oldText, string newText, bool ignoreWhitespace)
         {
