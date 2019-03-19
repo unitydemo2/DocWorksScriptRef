2eea4bfd06ad1a20464dfc6c20c02f560f99ad08
adaftuar-rythmos <45761202+adaftuar-rythmos@users.noreply.github.com>
2019-03-19 13:35:24 +0530

    Create TestingAgain.cs

---
+++ DiffPlex/TestingAgain.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiffPlex
+{
+    
+    public class TestingAgain
+    {
+        /// <summary>
+        /// This method will sort list in given order 
+        /// </summary>
+        /// <param name="array">list to be sorted</param>
+        /// <param name="sortInAscOrder">Order of sorting.</param>
+        public List<int> SortList(List<int> array, bool sortInAscOrder)
+        {
+            if (sortInAscOrder)
+            {
+                array.Sort();
+            }
+            else
+            {
+                array.Sort();
+                array.Reverse();
+            }
+            return array;
+        }
+
+        
+        public void Add()
+        {
+            Console.Write("You called add method");
+        }
+    }
+}
