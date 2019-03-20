b82a0ec7c8d5cbd4dd2bde27fd75b006bfb057b5
adaftuar-rythmos <45761202+adaftuar-rythmos@users.noreply.github.com>
2019-03-20 15:54:31 +0530

    Create Sample.cs

---
+++ DiffPlex/Sample.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using DiffPlex.Model;
+
+namespace DiffPlex
+{
+    
+    public class Sample
+    {
+       
+        public string PropertyForTesting { get; set; }
+
+        
+        public DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace)
+        {
+            return CreateLineDiffs(oldText, newText, ignoreWhitespace, false);
+        }
+    }
+}
