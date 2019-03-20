6ec96cfae55354abbef0a4f10283bb610ee2bd70
CMSQATeam <cmsqa123@gmail.com>
2019-03-20 16:13:27 +0530

    DW CMS Message SampleNew.cs

--- DiffPlex/SampleNew.cs
+++ DiffPlex/SampleNew.cs
@@ -5,11 +5,16 @@ using DiffPlex.Model;
 namespace DiffPlex
 {
     
+    /// <summary>
+    /// Test summary
+    /// </summary>
     public class SampleNew
     {
        
+        
         public string PropertyForTesting { get; set; }
 
+        
         
         public DiffResult CreateLineDiffs(string oldText, string newText, bool ignoreWhitespace)
         {
